48a9252f38bc6c383c39cace61264b0d4888cf18
oblakov0372 <demaoblakov1@gmail.com>
2022-06-11 21:02:14 +0300

    Delete using which we don't use

--- OOPHomeWork2/Interface/IFood.cs
+++ OOPHomeWork2/Interface/IFood.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOPHomeWork2
+﻿namespace OOPHomeWork2
 {
     interface IFood
     {
